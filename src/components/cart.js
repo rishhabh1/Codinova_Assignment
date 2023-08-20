2649690b4142f900595cfa30b2ca365c531d24df
rishabh <rish.khera86@gmail.com>
2023-08-21 00:32:58 +0530

    grand_total_calc

--- src/components/cart.js
+++ src/components/cart.js
@@ -32,57 +32,92 @@ export default function Cart(props) {
   const calculateTotalQuantity = () => {
     return cart.reduce((totalQty, item) => totalQty + item.quantity, 0);
   };
-  return (
-    <div className="cart_page">
-      <table>
-        <thead>
-          <tr>
-            <td>{String_Constants.products}</td>
-            <td>{String_Constants.price}</td>
-            <td>{String_Constants.qty}</td>
-            <td>{String_Constants.total}</td>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.length === 0 ? (
+
+
+    const [discountPercentage, setDiscountPercentage] = useState(0);
+    const [vatPercentage, setVatPercentage] = useState(0);
+    const calculateDiscount = () => {
+      return (calculateSubtotal() * discountPercentage) / 100;
+    };
+
+    const calculateVAT = () => {
+      return (calculateSubtotal() * vatPercentage) / 100;
+    };
+
+    const calculateGrandTotal = () => {
+      return calculateSubtotal() + calculateVAT() - calculateDiscount();
+    };
+
+    console.log(vatPercentage);
+    return (
+      <div className="cart_page">
+        <table>
+          <thead>
             <tr>
-              <td>{String_Constants.no_products}</td>
+              <td>{String_Constants.products}</td>
+              <td>{String_Constants.price}</td>
+              <td>{String_Constants.qty}</td>
+              <td>{String_Constants.total}</td>
             </tr>
-          ) : (
-            cart.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>
-                  <button onClick={() => handleDecreaseQuantity(index)}>
-                    -
-                  </button>
-                  {item.quantity}
-                  <button onClick={() => handleIncreaseQuantity(index)}>
-                    +
-                  </button>
-                </td>
-                <td>${(item.price * item.quantity).toFixed(2)}</td>
-                {/* <td>{item.total}</td> */}
-                <td>
-                  <button onClick={() => handleRemoveItem(index)}>
-                    Delete
-                  </button>
-                </td>
+          </thead>
+          <tbody>
+            {cart.length === 0 ? (
+              <tr>
+                <td>{String_Constants.no_products}</td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+            ) : (
+              cart.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <button onClick={() => handleDecreaseQuantity(index)}>
+                      -
+                    </button>
+                    {item.quantity}
+                    <button onClick={() => handleIncreaseQuantity(index)}>
+                      +
+                    </button>
+                  </td>
+                  <td>${(item.price * item.quantity).toFixed(2)}</td>
+                  {/* <td>{item.total}</td> */}
+                  <td>
+                    <button onClick={() => handleRemoveItem(index)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
 
-      <div className="cart-subtotal">
-        Subtotal: ${calculateSubtotal().toFixed(2)}
-      </div>
+        <div className="cart-subtotal">
+          Subtotal: ${calculateSubtotal().toFixed(2)}
+        </div>
 
-      <div className="cart-total-quantity">
-        Total Quantity: {calculateTotalQuantity()}
+        <div className="cart-total-quantity">
+          Total Quantity: {calculateTotalQuantity()}
+        </div>
+        <div className="cart-discount">
+          <input
+            type="number"
+            value={discountPercentage}
+            onChange={(e) => setDiscountPercentage(e.target.value)}
+          />
+          % Discount: ${calculateDiscount().toFixed(2)}
+        </div>
+        <div className="cart-vat">
+          <input
+            type="number"
+            value={vatPercentage}
+            onChange={(e) => setVatPercentage(e.target.value)}
+          />
+          % VAT: ${calculateVAT().toFixed(2)}
+        </div>
+        <div className="cart-grand-total">
+          Grand Total: ${calculateGrandTotal().toFixed(2)}
+        </div>
       </div>
-      <div className="cart-amount"></div>
-    </div>
-  );
+    );
 }
